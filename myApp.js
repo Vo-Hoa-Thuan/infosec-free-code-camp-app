f15fc989a7c8079ee6414762be7fa442deebdcca
Vo-Hoa-Thuan <vohoathuan2828@gmail.com>
2024-05-08 18:36:24 +0700

    t

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 // Khởi động server
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
